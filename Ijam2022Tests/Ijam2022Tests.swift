d0dc1c7f989a7de056ac1df2694baf349fbe8025
Ron Jurincie <ronjurincie@gmail.com>
2022-06-20 09:32:02 -0400

    added ViewInspector as dependency

---
+++ Ijam2022Tests/Ijam2022Tests.swift
@@ -0,0 +1,19 @@
+//
+//  Ijam2022Tests.swift
+//  Ijam2022Tests
+//
+//  Created by Ron Jurincie on 6/20/22.
+//
+
+import XCTest
+import ViewInspector
+@testable import Ijam2022
+
+class Ijam2022Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+
+}
