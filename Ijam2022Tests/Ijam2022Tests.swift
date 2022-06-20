37dcfa87c54f8bc95269daaca1ee95189c5766f7
Ron Jurincie <ronjurincie@gmail.com>
2022-06-20 09:34:36 -0400

    added new build version #

--- Ijam2022Tests/Ijam2022Tests.swift
+++ Ijam2022Tests/Ijam2022Tests.swift
@@ -14,6 +14,12 @@ class Ijam2022Tests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
-
+    
+    func testContentViewExists() {
+        let view = ContentView()
+        
+        XCTAssertNotNil(view)
+    }
+    
+    
 }
